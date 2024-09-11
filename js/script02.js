56c13a1b577ba27890ed19c19a0dda73c8b84f01
Jeremy Lambrecq <Jeremy.lmbq@hotmail.com>
2024-09-11 18:25:53 +0200

    JavaScriptEcole

---
+++ js/script02.js
@@ -0,0 +1,23 @@
+function maFonction() {
+    console.log("Hello")
+    }
+
+function carre(nombre) {
+    console.log("Le carré de ",nombre," est ",nombre*nombre);
+    }
+
+function carre2(nombre) {
+    return nombre*nombre;
+    }
+
+function multiplication(nombreA,nombreB) {
+    let resultat= nombreA*nombreB;
+    return resultat;
+    }
+
+const hello = function(nomEtudiant) {
+    const message = "Hello "+ nomEtudiant +" !";
+    return message;
+    }
+
+
